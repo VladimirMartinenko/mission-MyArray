60b13786006a972500ec49c0be1660e9b09cb58c
Vladimir Martinenko <teslabest87@gmail.com>
2022-02-20 19:45:44 +0200

    add unshift,reverse,concat

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,53 +16,82 @@ function MyArray() {
       return undefined;
     }
   };
-  /*this.forEach = function () {
+  this.forEach = function () {
     for (let i = 0; i < this.length; i++) {
       console.log(`${i}:${this[i]}`);
-      
+    }
   };
-}*/
   this.forEach = function (callback) {
     for (let i = 0; i < this.length; i++) {
-      callback(this[i],i,this);
+      callback(this[i], i, this);
     }
   };
   this.map = function (callback) {
-    let result = new MyArray()
+    let result = new MyArray();
     for (let i = 0; i < this.length; i++) {
-      let current = callback(this[i],i,this);
+      let current = callback(this[i], i, this);
       result.push(current);
     }
-        return result;
+    return result;
   };
   this.shift = function () {
     let meaning = this[0];
     delete this[0];
     this.length--;
-    for (let i = 0; i < this.length; i++){
-      this[i] =  this[i+1];
-      
+    for (let i = 0; i < this.length; i++) {
+      this[i] = this[i + 1];
     }
     delete this[this.length];
     return meaning;
-  }
-  this.unshift = function () {
-    
-    for (let i = 0; i < this.length; i++){
-      
-        this[i] =  this[i+1];
-        }
-  }
+  };
+  this.unshift = function (...rst) {
+    if (rst.length === 0) {
+      throw new RangeError("Введите значение");
+    }
+    for (let i = this.length - 1 + rst.length; i > 0; i--) {
+      this[i] = this[i - rst.length];
+    }
+    for (let i = 0; i < rst.length; i++) {
+      this[i] = rst[i];
+    }
+    this.length += rst.length;
+    return this.length;
+  };
+  this.reverse = function () {
+    for (let i = 0; i < this.length / 2; i++) {
+      let meaning = this[i];
+      this[i] = this[this.length - 1 - i];
+      this[this.length - 1 - i] = meaning;
+    }
+    return this;
+  };
+  this.concat = function (arr2) {
+    let newArr = new MyArray();
+    for (let i = 0; i < this.length; i++) {
+      newArr[i] = this[i];
+    }
+    for (let i = 0; i < arr2.length; i++) {
+      newArr[i + this.length] = arr2[i];
+    }
+    newArr.length = this.length + arr2.length;
+    return newArr;
+  };
 }
 const arr = new MyArray();
 arr.push(2);
 arr.push(4);
 arr.push(5);
-arr.forEach(callback);
+
+const arr5 = new MyArray();
+arr5.push(3);
+arr5.push(7);
+arr5.push(11);
+
+/*arr.forEach(callback);
 function callback(i, item, arr) {
   console.log(i, item, arr);
 }
 const newArr = arr.map(callback);
 function callback(i, item, arr) {
   return i,item;
-  }
+  }*/
